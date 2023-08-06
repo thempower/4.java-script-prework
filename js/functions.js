1f6c082da44719b68387bcb9efbbc8aff6f810b7
Krzysztof Staszewski <krzysiek.staszewski@gmail.com>
2023-08-06 21:28:51 +0200

    finished js function and logic. css styling left

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,76 @@
-'use strict';
-
-
-function printMessage(msg){
-	var div = document.createElement('div');
+function printMessage(msg) {
+	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
+function clearMessages() {
 	document.getElementById('messages').innerHTML = '';
+}
+
+/**
+ * Draw random computer move and retuns move name as a string.
+ */
+function getCompMove() {
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  if (randomNumber == 1) {
+    return 'rock';
+  } else if (randomNumber == 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+/**
+ * Makes decision about result of the game and displays on the screen.
+ */
+function playGame(playerMove) {
+  let computerMove = getCompMove();
+  clearMessages();
+  if (playerMove == 'paper' && computerMove == 'rock') {
+    printMessage('YOU WON!');
+    scorePlayer++;
+  } else if (playerMove == 'rock' && computerMove == 'scissors') {
+    printMessage('YOU WON!');
+    scorePlayer++;
+  } else if (playerMove == 'scissors' && computerMove == 'paper') {
+    printMessage('YOU WON!');
+    scorePlayer++;
+  } else if (playerMove == computerMove) {
+    printMessage('REMIS');
+  } else {
+    printMessage('YOU LOSE');
+    scoreComputer++;
+  }
+  counter++;
+  printMessage('comp had ' + computerMove + ', you had ' + playerMove);
+  printMessage('round: ' + counter);
+  printMessage('scoreboard:')
+  printMessage('human: ' + scorePlayer + ' / computer: ' + scoreComputer);
+  printMessage('play again!');
+}
+
+/**
+ * Reaction to reset button clicked by user. Clear scores and round counter.
+ */
+function resetCounter() {
+  scorePlayer = 0;
+  scoreComputer = 0;
+  counter = 0;
+  clearMessages();
+  printMessage('reset');
+  printMessage('counter clear');
+  printMessage('scoreboard clear');
+  }
+
+/**
+ * Reaction to button clicked by user.
+ */
+function buttonClicked(playerMove) {
+  if (playerMove == 'reset') {
+    resetCounter();
+  } else {
+    playGame(playerMove);
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,34 +1,14 @@
-'use strict';
+const buttonScissors = document.getElementById('button-scissors'),
+      buttonPaper = document.getElementById('button-paper'),
+      buttonRock = document.getElementById('button-rock'),
+      buttonReset = document.getElementById('button-reset');
 
+let scorePlayer = 0, scoreComputer = 0, counter = 0;
 
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+buttonRock.addEventListener('click', function(){ buttonClicked('rock'); });
 
-//computer move
-var randomNumber = Math.floor(Math.random() * 3 + 1);
-if (randomNumber == '1') {
-  computerMove = 'rock';
-} else if (randomNumber == '2') {
-  computerMove = 'paper';
-} else if (randomNumber == '3') {
-  computerMove = 'scissors';
-} else
-  computerMove = 'unkown move';
+buttonPaper.addEventListener('click', function(){ buttonClicked('paper'); });
 
-  printMessage('Computer move: ' + computerMove);
+buttonScissors.addEventListener('click', function(){ buttonClicked('scissors'); });
 
-//player move
-  playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-  console.log('Wpisana odpowiedź to: ' + playerInput);
-  if (playerInput == '1') {
-    playerMove = 'rock';
-  } else if (playerInput == '2') {
-    playerMove = 'paper';
-  } else if (playerInput == '3') {
-    playerMove = 'scissors';
-  } else
-    playerMove = 'unkown move';
-
-  printMessage('Player move: ' + playerMove);
-
-
-
+buttonReset.addEventListener('click', function(){ buttonClicked('reset'); });
